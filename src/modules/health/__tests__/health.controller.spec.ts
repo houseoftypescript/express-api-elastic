942918ecd996937ec619425ab2953e6951914284
hieudoanm <hieumdoan@gmail.com>
2023-05-27 18:21:35 +0700

    Update Test

--- src/modules/health/__tests__/health.controller.spec.ts
+++ src/modules/health/__tests__/health.controller.spec.ts
@@ -1,11 +1,17 @@
 import { HealthController } from '../health.controller';
 
+jest.mock('../../../common/libs/elastic', () => {
+  return {
+    elasticSearchClient: { healthCheck: jest.fn().mockResolvedValue({ status: 'OK' }) },
+  };
+});
+
 describe('HealthController', () => {
   const healthController: HealthController = new HealthController();
 
   describe('/health', () => {
     it('should return status OK', async () => {
-      const response = healthController.getHealth();
+      const response = await healthController.getHealth();
       expect(response).toEqual({ status: 'OK' });
     });
   });
